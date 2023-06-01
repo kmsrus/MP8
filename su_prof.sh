5a24d5ac493ed8496300c15e9f50bc505106b05e
kmsrus <kmsrussia@yandex.ru>
2023-06-01 11:40:27 +0300

    новая версия скрипта, добалнена возможность записи имени пользоавтеля в переменную

--- su_prof.sh
+++ su_prof.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-# Проверяем есть ли уже такие строки в файле sudoers. Вносим измеения если строки не найдены
 
+# Проверяем есть ли уже такие строки в файле sudoers. Вносим измеения если строки не найдены
+read -p "Введите имя пользователся: " USER_NEW
 if ! sudo grep -q "^Cmnd_Alias MPROOTCMD =" /etc/sudoers; then
     # Если строка не найдена, добавляем ее в файл
     echo "Cmnd_Alias MPROOTCMD = /usr/bin/at -l, /usr/bin/crontab * -l, /sbin/fdisk -l, /sbin/iptables-save, /bin/netstat, /bin/ss, /usr/sbin/dmidecode, /sbin/vgdisplay, /sbin/lvdisplay, /sbin/auditctl -[lv]" | sudo tee -a /etc/sudoers
@@ -18,54 +19,59 @@ if ! sudo grep -q "env_reset" /etc/sudoers; then
     echo "Defaults  env_reset" | sudo tee -a /etc/sudoers
 fi
 
-if ! sudo grep -q "^$USER ALL = (ALL) NOPASSWD: MPROOTCMD" /etc/sudoers; then
-    echo "$USER ALL = (ALL) NOPASSWD: MPROOTCMD" | sudo tee -a /etc/sudoers
+
+
+if ! sudo grep -q "^$USER_NEW ALL = (ALL) NOPASSWD: MPROOTCMD" /etc/sudoers; then
+    echo "$USER_NEW ALL = (ALL) NOPASSWD: MPROOTCMD" | sudo tee -a /etc/sudoers
 fi
 
-if ! sudo grep -q "^$USER ALL = (ALL) NOPASSWD: MPFILECMD" /etc/sudoers; then
-    echo "$USER ALL = (ALL) NOPASSWD: MPFILECMD" | sudo tee -a /etc/sudoers 
+if ! sudo grep -q "^$USER_NEW ALL = (ALL) NOPASSWD: MPFILECMD" /etc/sudoers; then
+    echo "$USER_NEW ALL = (ALL) NOPASSWD: MPFILECMD" | sudo tee -a /etc/sudoers 
 fi 
 
-if ! sudo grep -q "^$USER ALL = (ALL) NOPASSWD: MPEXCPTNSCMD" /etc/sudoers; then
-    echo "$USER ALL = (ALL) NOPASSWD: MPEXCPTNSCMD" | sudo tee -a /etc/sudoers
+if ! sudo grep -q "^$USER_NEW ALL = (ALL) NOPASSWD: MPEXCPTNSCMD" /etc/sudoers; then
+    echo "$USER_NEW ALL = (ALL) NOPASSWD: MPEXCPTNSCMD" | sudo tee -a /etc/sudoers
 fi
 
 echo "sudoers настроено"
+DIRECTORIA=/home/$USER_NEW/bin
 # Проверяем существует ли директория $HOME/bin если нет то создаем
-if [ -d "$HOME/bin" ] ; then
-echo "Директория существует $HOME/bin"
+if [ -d "$DIRECTORIA" ] ; then
+  echo "Директория существует $DIRECTORIA"
 else
- mkdir -p $HOME/bin
+  sudo mkdir -p $DIRECTORIA
 fi
 # Проверяетм права доступа к домашней директории, должен быть равен 700
 
-if [[ "$(stat -c '%a' "$HOME")" != "700" ]]; then
+if [[ "$(stat -c '%a' "/home/$USER_NEW")" != "700" ]]; then
   # Если права доступа отличаются от 700, то изменяем их
-  chmod 700 -R "$HOME"
-  echo "Установлены права доступа 700 для директории $HOME"
+  sudo chmod 700 -R "/home/$USER_NEW"
+  echo "Установлены права доступа 700 для директории /home/$USER_NEW/"
 else
-  echo "Права доступа для директории $HOME уже установлены на 700"
+  echo "Права доступа для директории /home/$USER_NEW уже установлены на 700"
 fi
 
 # Указываем директорию, которую нужно добавить в PATH
-NEW_DIR_BIN="$HOME/bin"
 
 if ! sudo grep -q "^export PATH=" ~/.bashrc; then
-    echo "export PATH=$NEW_DIR_BIN:$PATH" | sudo tee -a ~/.bashrc
+    echo "export PATH=$DIRECTORIA:$PATH" | sudo tee -a ~/.bashrc
     #source ~/.bashrc
-    export PATH=$NEW_DIR_BIN:$PATH
-    echo "Добавлена директория $NEW_DIR_BIN в PATH"
+    export PATH=$DIRECTORIA:$PATH
+    echo "Добавлена директория $DIRECTORIA в PATH"
     echo $PATH
 else
     # Если директория уже есть в PATH, выводим сообщение об этом
-   echo "Директория $NEW_DIR_BIN уже есть в PATH"
+   echo "Директория $DIRECTORIA уже есть в PATH"
 fi
 
 # Распаковываем содержимое архива bin.tar в директорию NEW_DIR_BIN
-if [ -f "$NEW_DIR_BIN/.soft" ]; then
-    echo "bit.tar уже распакован в $NEW_DIR_BIN"
+if [ -f "$DIRECTORIA/.soft" ]; then
+    echo "bit.tar уже распакован в $DIRECTORIA"
 else 
-tar -xvf $HOME/bin.tar -C $NEW_DIR_BIN >> $NEW_DIR_BIN/.soft
-mv $NEW_DIR_BIN/bin/* $NEW_DIR_BIN/
-rm -r $NEW_DIR_BIN/bin
+FIND_ARCHIV=$(sudo find /home/  -name bin.tar)
+ARCHIV=$(echo $FIND_ARCHIV | cut -d' ' -f1)
+
+sudo tar -xvf $ARCHIV -C $DIRECTORIA >> $DIRECTORIA/.soft
+sudo mv $DIRECTORIA/bin/* $DIRECTORIA/
+sudo rm -r $DIRECTORIA/bin
 fi 
